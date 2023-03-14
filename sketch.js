f26449f9e282a189bb3ae0628c5b629012d87b1a
koeefu <keyifu99@gmail.com>
2023-03-14 13:59:50 -0400

    final

--- sketch.js
+++ sketch.js
@@ -1,11 +1,7 @@
-let x_chg;
-let y_chg;
-let rot = 0;
 let h1 = 0;
 let h2;
 
 let apples = [];
-
 
 function setup() {
   let windowSize = min(windowWidth, windowHeight);
@@ -60,7 +56,7 @@ function draw() {
   ellipse(380, 360, 20); //tail
   fill(255, 244, 218);
   ellipse(379, 358, 15);
-  //rot ++;
+  
 
   fill(255, 244, 218);
   triangle(265, 290, 255, 320, 275, 310); //left ear
@@ -83,8 +79,8 @@ function draw() {
     ellipse(258, 345, 33); //bigger one
     ellipse(308, 345, 33);
     fill(85, 71, 37);
-    x_chg = map(mouseX, 0, 400, -3, 3);
-    y_chg = map(mouseY, 0, 400, -3, 3);
+    let x_chg = map(mouseX, 0, 400, -3, 3);
+    let y_chg = map(mouseY, 0, 400, -3, 3);
     ellipse(258 + x_chg, 345 + y_chg, 22);
     ellipse(308 + x_chg, 345 + y_chg, 22);
   }
