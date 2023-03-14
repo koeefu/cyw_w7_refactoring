a7b2e9c89a60404c31de3cb5ce4113abed68dab8
koeefu <keyifu99@gmail.com>
2023-03-14 14:21:44 -0400

    kitten function

--- sketch.js
+++ sketch.js
@@ -44,46 +44,9 @@ function draw() {
     apples[i].move();
   }
 
-  //KITTEN
-  colorMode(RGB);
-  //enlarge the kitten
-  translate(-280, -300);
-  scale(1.7);
+  //kitten
+  kitten();
 
-  noStroke();
-
-  fill(255, 234, 185);
-  ellipse(380, 360, 20); //tail
-  fill(255, 244, 218);
-  ellipse(379, 358, 15);
-  
-
-  fill(255, 244, 218);
-  triangle(265, 290, 255, 320, 275, 310); //left ear
-  triangle(313, 290, 305, 310, 325, 320);
-  rect(221, 350, 166, 85, 40, 40, 40, 45); //body
-
-  fill(255, 238, 197);
-  rect(230, 310, 116, 70, 33); //head
-  fill(255, 244, 218);
-  rect(230, 310, 106, 68, 33);
-
-  noStroke();
-  if (mouseIsPressed) {
-    stroke(85, 71, 37);
-    strokeWeight(8);
-    line(258 - 8, 348, 258 + 8, 344);
-    line(308 - 8, 344, 308 + 8, 348);
-  } else {
-    fill(236, 248);
-    ellipse(258, 345, 33); //bigger one
-    ellipse(308, 345, 33);
-    fill(85, 71, 37);
-    let x_chg = map(mouseX, 0, 400, -3, 3);
-    let y_chg = map(mouseY, 0, 400, -3, 3);
-    ellipse(258 + x_chg, 345 + y_chg, 22);
-    ellipse(308 + x_chg, 345 + y_chg, 22);
-  }
   pop();
 }
 
@@ -110,5 +73,47 @@ class appleBG {
     if (this.x > width) {
       this.x -= 1000;
     }
+  }
+}
+
+function kitten() {
+  colorMode(RGB);
+  //enlarge the kitten
+  translate(-280, -300);
+  scale(1.7);
+
+  noStroke();
+
+  fill(255, 234, 185);
+  ellipse(380, 360, 20); //tail
+  fill(255, 244, 218);
+  ellipse(379, 358, 15);
+
+  fill(255, 244, 218);
+  triangle(265, 290, 255, 320, 275, 310); //left ear
+  triangle(313, 290, 305, 310, 325, 320);
+  rect(221, 350, 166, 85, 40, 40, 40, 45); //body
+
+  fill(255, 238, 197);
+  rect(230, 310, 116, 70, 33); //head
+  fill(255, 244, 218);
+  rect(230, 310, 106, 68, 33);
+
+  noStroke();
+
+  if (mouseIsPressed) {
+    stroke(85, 71, 37);
+    strokeWeight(8);
+    line(258 - 8, 348, 258 + 8, 344);
+    line(308 - 8, 344, 308 + 8, 348);
+  } else {
+    fill(236, 248);
+    ellipse(258, 345, 33); //bigger one
+    ellipse(308, 345, 33);
+    fill(85, 71, 37);
+    let x_chg = map(mouseX, 0, 400, -3, 3);
+    let y_chg = map(mouseY, 0, 400, -3, 3);
+    ellipse(258 + x_chg, 345 + y_chg, 22);
+    ellipse(308 + x_chg, 345 + y_chg, 22);
   }
 }
