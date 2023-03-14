8ec426a7edaf12b46082cdff1eeb1a31461dadca
koeefu <keyifu99@gmail.com>
2023-03-14 13:02:02 -0400

    new initial

--- sketch.js
+++ sketch.js
@@ -1,121 +1,118 @@
-let angle = 0;
-let angle2 = 0;
-let angle3 = 0;
-let sd = 0.5;
-let sd2 = 0.3;
-let sd3 = 10;
-let spacing = 200;
-let sizeVase = 1;
-let num1 = 3;
-let num2 = 4;
-
-let currentSeed = 0;
+let e_xpos = 35;
+let e_ypos = 60;
+let step = 100;
+let x_chg;
+let y_chg;
+let rot = 0;
+let h1 = 0;
+let h2;
 
 function setup() {
-  colorMode(HSB);
-  createCanvas(600, 600);
+  createCanvas(400, 400);
   angleMode(DEGREES);
-  background("white");
+  if (h1 == 0) {
+    h1 = random(0, 360);
+    h2 = random(0, 222);
+  }
 }
 
 function draw() {
-  backColor = color("#E4E4E4");
-  backColor.setAlpha(0.02);
-  background(backColor);
+  colorMode(HSB);
 
-  randomSeed(currentSeed);
+  background(h2, 65, 90);
 
-  scale(sizeVase);
-
-  for (let i = 0; i < num1; i++) {
-    for (let j = 0; j < num1; j++) {
-      push();
-      translate(0 + i * spacing, 0 + j * spacing);
-      drawVase();
-      pop();
+  noStroke();
+  // the below coding is nested loop
+  // Apple
+  fill(h1, 29, 100);
+  for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < 4; i++) {
+      ellipse(
+        e_xpos + 100 * j + (frameCount % 400), 
+        e_ypos + step * i, 
+        50, 
+        60
+      );
+      ellipse(
+        e_xpos + 30 + 100 * j + (frameCount % 400),
+        e_ypos + step * i,
+        50,
+        60
+      );
+      ellipse(
+        -e_xpos - 100 * j + (frameCount % 400),
+        e_ypos + step * i,
+        50,
+        60
+      );
+      ellipse(
+        -e_xpos - 30 - 100 * j + (frameCount % 400),
+        e_ypos + step * i,
+        50,
+        60
+      );
     }
   }
 
-  for (let x = 0; x < num2; x++) {
-    for (let y = 0; y < num2; y++) {
-      push();
-      translate(0 + x * spacing, 0 + y * spacing);
-      drawDeco();
-      pop();
+  for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < 4; i++) {
+      triangle(
+        50 + i * step + (frameCount % 400),
+        30 + j * step,
+        45 + i * step + (frameCount % 400),
+        17 + j * step,
+        55 + i * step + (frameCount % 400),
+        17 + j * step
+      );
+      triangle(
+        -50 - i * step + (frameCount % 400),
+        30 + j * step,
+        -45 - i * step + (frameCount % 400),
+        17 + j * step,
+        -55 - i * step + (frameCount % 400),
+        17 + j * step
+      );
     }
   }
-}
 
-function mousePressed() {
-  currentSeed = random(999999);
-  //refresh my canvas
+  //KITTEN
+  colorMode(RGB);
+  //enlarge the kitten
+  translate(-280, -300);
+  scale(1.7);
+
   noStroke();
-  fill(255);
-  rect(0, 0, width * num1, height * num1);
 
-  //make the vase get smaller
-  push();
-  sizeVase = sizeVase / 2;
-  num1 = num1 * 2;
-  num2 = num2 * 2;
-  sd /= 4;
-  sd2 /= 4;
-  sd3 /= 4;
-  pop();
-}
+  fill(255, 234, 185);
+  ellipse(380, 360, 20); //tail
+  fill(255, 244, 218);
+  ellipse(379, 358, 15);
+  //rot ++;
 
-function drawVase() {
-  /******************* base *******************/
-  push();
+  fill(255, 244, 218);
+  triangle(265, 290, 255, 320, 275, 310); //left ear
+  triangle(313, 290, 305, 310, 325, 320);
+  rect(221, 350, 166, 85, 40, 40, 40, 45); //body
+
+  fill(255, 238, 197);
+  rect(230, 310, 116, 70, 33); //head
+  fill(255, 244, 218);
+  rect(230, 310, 106, 68, 33);
+
   noStroke();
-  fill(random(360), 54, 86, 25);
-  quad(108, 170, 92, 170, 70, 188, 130, 188);
-  line(0, 0, 0, 40);
-  pop();
-
-  /************** the body part ***************/
-  push();
-  translate(100, 107);
-  rotate(angle2);
-  stroke(random(360), 54, 86);
-  line(0, 0, 0, 42);
-  angle2 += sd2;
-  pop();
-
-  push();
-  translate(100, 145);
-  rotate(angle2);
-  stroke(random(360), 54, 86);
-  line(0, 0, 0, 25);
-  angle2 += sd2;
-  pop();
-
-  /******************** head ********************/
-  push();
-  translate(100, 45);
-  rotate(angle);
-  stroke(random(360), 54, 86);
-  line(0, 0, 0, -30);
-  if (angle > 60 || angle < -60) {
-    sd = -sd;
+  if (mouseIsPressed) {
+    stroke(85, 71, 37);
+    strokeWeight(8);
+    line(258 - 8, 348, 258 + 8, 344);
+    line(308 - 8, 344, 308 + 8, 348);
+  } else {
+    fill(236, 248);
+    ellipse(258, 345, 33); //bigger one
+    ellipse(308, 345, 33);
+    fill(85, 71, 37);
+    x_chg = map(mouseX, 0, 400, -3, 3);
+    y_chg = map(mouseY, 0, 400, -3, 3);
+    ellipse(258 + x_chg, 345 + y_chg, 22);
+    ellipse(308 + x_chg, 345 + y_chg, 22);
   }
-  angle += sd;
-  pop();
-
-  /****************** neck **********************/
-  noStroke();
-  fill(random(360), 54, 86, 25);
-  ellipse(100, 45, 20, 8);
-  ellipse(100, 55, 40, 15);
-  ellipse(100, 65, 20, 8);
-}
-
-function drawDeco() {
-  push();
-  translate(0, 100);
-  rotate(angle2);
-  stroke("#FDE50E");
-  line(0, 12, -12, 0);
-  angle3 += -sd3;
-  pop();
 }
